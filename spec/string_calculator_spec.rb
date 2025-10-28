0b1dbeeec15dd53e58401584adf356f4341f51fd
Saikanhaiya21 <80808406+Saikanhaiya21@users.noreply.github.com>
2025-10-28 09:46:12 +0000

    Add first tests for empty, single and multiple number strings

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,25 @@
 require './string_calculator'
 
 RSpec.describe StringCalculator do
+  describe '.add' do
+    context 'when input is empty' do
+      it 'returns 0' do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+
+    context 'when input contains a single number' do
+      it 'returns the number itself' do
+        expect(StringCalculator.add("1")).to eq(1)
+        expect(StringCalculator.add("2")).to eq(2)
+      end
+    end
+
+    context 'when input contains multiple numbers' do
+      it 'returns the sum of the numbers' do
+        expect(StringCalculator.add("1,5")).to eq(6)
+        expect(StringCalculator.add("1,0")).to eq(1)
+      end
+    end
+  end
 end
